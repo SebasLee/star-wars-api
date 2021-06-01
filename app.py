203b47ac6bbe22f2d10cdb828dc2200e15cfaecb
SebasLee <heechanlee98@gmail.com>
2021-06-01 13:51:27 -0700

    stuff added

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+films = [
+    {
+        'title': 'A New Hope',
+        'details': [
+            {
+                'hero': 'Luke Skywalker',
+                'villain': 'Darth Vader'
+            }
+        ]
+    }
+]
+
+@app.route('/')
+def home(): 
+    return "Hello World"
+
+# GET /films
+@app.route('/film')
+def get_films():
+    return jsonify({'films': films})
+
+app.run(port=5000)
